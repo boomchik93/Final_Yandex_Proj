d5e681c0827461556fb425c9c32d2706c84ab7c7
boomchik93 <fedyazhukov06@gmail.com>
2025-04-26 22:54:28 +0500

    update
    added:
    base.html
    logo and avatar pics
    reworked html pages

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-# app.py (обновленная версия)
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 
+# Существующие маршруты
 @app.route('/')
 def home():
     return render_template('main.html')
@@ -23,6 +23,21 @@ def register():
 @app.route('/profile')
 def profile():
     return render_template('lk.html')
+
+
+@app.route('/orders')
+def orders():
+    return render_template('orders.html')
+
+
+@app.route('/payment')
+def payment():
+    return render_template('payment.html')
+
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
